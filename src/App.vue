20f423edec5124c2d4abe30ed9f20376737d5672
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-11-07 14:45:46 +0100

    add show list and delete option

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const input_content = ref('')
 const input_category = ref(null)
  
 const todos_asc = computed(() => todos.value.sort((a, b) => {
-    return b.createdAt - a.createdAt
+    return a.createdAt - b.createdAt
 }))
 
 const addTodo = () => {
@@ -18,10 +18,17 @@ const addTodo = () => {
 
   todos.value.push({
     content: input_content.value,
-    cattegory: input_category.value,
+    category: input_category.value,
     done: false,
-    createdAt: new Date().geTime()
+    createdAt: new Date().getTime()
   })
+ 
+  input_content.value = ''
+  input_category.value = null
+}
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 
 watch(todos, newVal => {
@@ -82,6 +89,23 @@ onMounted(() => {
 
         <input type="submit" value="add todo">
       </form>
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
